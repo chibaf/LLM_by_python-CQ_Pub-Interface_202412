3107fb0cbe5139281d5cb8c68c1faedaac09173e
chibaf <chibaf@mac.com>
2024-10-25 23:43:08 +0900

    Add files via upload

---
+++ s_rd.py
@@ -0,0 +1,17 @@
+def read_data(file_path):
+    # 学習データセットを保持するリスト
+# https://github.com/csebuetnlp/xl-sum?tab=readme-ov-file#datasets
+#    import json
+    texts = []
+    summaries = []
+
+    # JSONLファイルから text と summary を抽出
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            json_data = json.loads(line)
+            text = json_data.get("text")
+            summary = json_data.get("summary")
+            if text and summary:
+                texts.append(text)
+                summaries.append(summary)
+    return texts, summaries
