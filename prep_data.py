62dbddc329d4930418244c32fe85db1b6b763c90
chibaf <chibaf@mac.com>
2024-10-28 23:36:10 +0900

    Add files via upload

---
+++ prep_data.py
@@ -0,0 +1,28 @@
+import torch
+import s_rd
+from tokenizer_class import Tokenizer 
+
+# confirming correctly reading data
+file_path_train='japanese_train.jsonl'
+file_path_val='japanese_val.jsonl'
+
+# reading data for AI generation of summary (for learning)
+texts,summaries=s_rd.read_data(file_path_train)
+train_data=""
+for text, summary in zip(texts,summaries):
+  train_data=train_data+"<BOS>"+text+"<SUMMARY>"+summary+"<EOS>"
+vocab=Tokenizer.create_vocab(train_data)
+tokenizer=Tokenizer(vocab)
+vocab_size=len(vocab)
+
+# reading data for AI generation of summary (for estimation/developing)
+texts,summaries=s_rd.read_data(file_path_val)
+val_data=""
+for text, symmary in zip(texts,summaries):
+  val_data=val_data+"<BOS>"+text+"<SUMMARY>"+summary+"<EOS>"
+
+train_data=torch.tensor(tokenizer.encode(train_data),dtype=torch.long)
+val_data=torch.tensor(tokenizer.encode(val_data),dtype=torch.long)
+
+print(train_data)
+print(val_data)
